9687b20607cd6754bf312a43996449ac1c5e194d
gdomnijl <gdomnijl@gmail.com>
2019-04-03 13:19:07 -0700

    scrap with multiple command lines

---
+++ scraper.sh
@@ -0,0 +1,6 @@
+twitterscraper nom -bd 2017-01-01 -ed 2017-01-31 --output=nom_jan.json
+twitterscraper nom -bd 2017-02-01 -ed 2017-02-28 --output=nom_feb.json
+## step 1: create this bash file (__.sh) with all the command line you want to run 
+## using any text editor
+## step 2: run this in terminal: chmod 755 scraper.sh
+## step 3: run this in terminal: ./scraper.sh
